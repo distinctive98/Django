a267dda35a72d341bf2c64198fbbe2a66e697a9e
distinctive98 <distinctive98@naver.com>
2019-10-31 17:32:15 +0900

    study SQLite3

--- DJANGO_BLOG/articles/urls.py
+++ DJANGO_BLOG/articles/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
 
 urlpatterns = [
-    path('<int:pk>/', views.detail),
-    path('create/', views.create),
-    path('new/', views.new),
-    path('', views.index),
+    path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/', views.detail, name="detail"),
+    path('create/', views.create, name="create"),
+    #path('new/', views.new),
+    path('', views.index, name="index"),
 ]
--- DJANGO_BLOG/articles/views.py
+++ DJANGO_BLOG/articles/views.py
@@ -21,14 +21,33 @@ def create(request):
     # article = Article(title=title, content=content)
     # article.save()
 
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-    article = Article(title=title, content=content)
-    article.save()
-
-    return redirect(f'/articles/{article.pk}/')
-
+    if request.method == 'POST':    
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return render(request, 'articles/new.html')
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
     return render(request, 'articles/detail.html', {'article':article})
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else :
+        return redirect('article:detail', article.pk)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return render(request, 'articles/edit.html', {'article':article})
